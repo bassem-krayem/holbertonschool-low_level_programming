50843528f53708d873f711b52632f308c3eccde3
bassem-krayem <bassem.krayem09@gmail.com>
2024-02-20 12:45:22 +0100

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-	/*
- * main - Prints a string
- * description:             prints a string
- *
- * Return allways 0
- */
+/**
+* main - Prints a string
+*
+* Return: allways 0.
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
