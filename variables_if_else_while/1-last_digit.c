4d22743e4c39a8c8070ab4fd0aca5093d349a510
bassem-krayem <bassem.krayem09@gmail.com>
2024-02-23 09:21:48 +0100

    task1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	int n;
-int x;
+	int x;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
@@ -21,7 +21,7 @@ int x;
 	}
 	else if (x == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, x);
+		printf("Last digit of %d is %d and is 0\n", n, x);
 	}
 	else
 	{
