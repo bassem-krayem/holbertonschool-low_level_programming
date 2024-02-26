aa1d3217e6963270226f2d2f65c28e37d8c90f2c
bassem-krayem <bassem.krayem09@gmail.com>
2024-02-26 13:55:45 +0100

    task0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the letter upper case or not
+ *@c: the character will be checked
+ *
+ * Return: Returns 1 if c is uppercase
+ * Returns 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
