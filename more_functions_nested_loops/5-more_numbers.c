16f89673a62f66560dc3b3a9dab73f3c31f58a26
bassem-krayem <bassem.krayem09@gmail.com>
2024-02-27 11:27:20 +0100

    task6

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 10 times
+ *
+ * Return: void.
+ */
+
+void more_numbers(void)
+{
+	int numbers;
+	int times;
+
+	for (times = 0; times <= 10; times++)
+	{
+		for (numbers = 0; numbers <= 14; numbers++)
+		{
+			if (numbers > 9)
+			{
+				_putchar(numbers / 10 + '0');
+				_putchar(numbers % +'0');
+			}
+		}
+	}
+	_putchar('\n');
+}
---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - prints _ in terminal
+ *
+ * Return (void).
+ */
+
+void print_line(int n)
+{
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		int line;
+		for (line = 0; line < n; line++)
+			_putchar('_');
+	}
+}
