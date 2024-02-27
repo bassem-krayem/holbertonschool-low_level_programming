3c468e9c48e5d378d5bbfb531ec08f69ac1764d3
bassem-krayem <bassem.krayem09@gmail.com>
2024-02-27 17:28:25 +0100

    task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle in the terminal
+ * @size: the size of the triangle,
+ *
+ * Return: void.
+ */
+
+void print_triangle(int size)
+{
+	int l, c;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (l = 1; l <= size; l++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				if (c < size - l)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
