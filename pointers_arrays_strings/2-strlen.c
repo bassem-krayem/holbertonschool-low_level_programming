d731a4533e8e63d44a1f8547a25403880f04165f
bassem-krayem <bassem.krayem09@gmail.com>
2024-02-28 14:19:09 +0100

    task2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - find the length of the string
+ * @s: the variable that holds the string.
+ *
+ * Return: 0;
+ */
+
+int _strlen(char *s)
+{
+
+	int _strlen(char *s)
+	{
+		int length = 0;
+
+		while (*s != '\0')
+		{
+			length++;
+			s++;
+		}
+		return (length);
+	}
