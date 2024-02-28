82b51caa221ae8e1ebe7ec77aed09ad546e15d5f
bassem-krayem <bassem.krayem09@gmail.com>
2024-02-28 14:25:17 +0100

    task2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -10,15 +10,12 @@
 
 int _strlen(char *s)
 {
+	int length = 0;
 
-	int _strlen(char *s)
+	while (*s != '\0')
 	{
-		int length = 0;
-
-		while (*s != '\0')
-		{
-			length++;
-			s++;
-		}
-		return (length);
+		length++;
+		s++;
 	}
+	return (length);
+}
