6c4dba0f8dc75e273327d3f2c1c2c562a72d8f7b
bassem-krayem <bassem.krayem09@gmail.com>
2024-02-28 13:35:54 +0100

    task1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap - swaping the values of two intigirs
+ * @a: the first variable.
+ * @b: the second variables to exchange with the first one.
+ *
+ * Return: void.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
