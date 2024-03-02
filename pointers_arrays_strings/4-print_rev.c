b7f615caf61fb220e360451862f77dece7e455dd
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-02 11:50:09 +0100

    task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: the string to be printed
+ */
+void print_rev(char *s)
+{
+	int length;
+	int i;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
