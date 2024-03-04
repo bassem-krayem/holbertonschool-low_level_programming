69a801bc080d0f96869ccf57a6c45daae246f59a
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-04 14:35:53 +0100

    task0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,17 @@
 #include "main.h"
 
+/**
+ * *_memset - function for falls
+ * @s: string.
+ * @b: the character
+ * @n: the intigir
+ * Return: s.
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	_memset(s + 0, b, n);
+
+	memset(s, b, n);
+
 	return (s);
 }
