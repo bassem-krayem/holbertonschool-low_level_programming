0c5f7167c302679e6dceed4619b5c7f83aedda26
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-07 14:38:41 +0100

    task2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - get the length of a string
+ * @s: the int that will hold the length.
+ * Return:
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (_strlen_recursion(s + 1) + 1);
+}
