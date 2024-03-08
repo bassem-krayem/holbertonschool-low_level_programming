e633df38a8452ae74092996172866348230a66fc
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-08 10:34:58 +0100

    task0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - prints the program name
+ * @argc: an unused variable.
+ * @argv: the array of strings contains the arguments.
+ *
+ * Return: 0.
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
