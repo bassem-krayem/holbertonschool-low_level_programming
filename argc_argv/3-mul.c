bfc8892b2f6ebf0e650bfb7d3ee5593678b45dfe
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-08 15:39:26 +0100

    task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - multiplie 2 numbers
+ * @argc: the arguments.
+ * @argv: the array of the strings.
+ * Return: 0.
+ */
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
