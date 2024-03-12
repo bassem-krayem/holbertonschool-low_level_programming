a33e30711c828deb617358fb231ec4f91542e918
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-12 16:29:59 +0100

    task4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2D grid created by alloc_grid function
+ * @grid: The grid to be freed
+ * @height: The height of the grid
+ *
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
