05d86de9957eff477e475d87f2bbad969d0e990f
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-12 15:58:16 +0100

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,15 +7,12 @@
  *
  * Return: The length of the string
  */
-
 int _strlen(char *s)
 {
 	int length = 0;
 
 	if (s == NULL)
-	{
-		return (0);
-	}
+		return 0;
 
 	while (s[length])
 	{
@@ -23,47 +20,38 @@ int _strlen(char *s)
 	}
 	return (length);
 }
-/**
- * *str_concat - continate 2 strings
- * @s1: string 1.
- * @s2: the string number2.
- * Return: the string.
- */
 
+/**
+ * *str_concat - Concatenate 2 strings
+ * @s1: String 1.
+ * @s2: String 2.
+ * Return: The concatenated string.
+ */
 char *str_concat(char *s1, char *s2)
 {
-	int length1, length2, i, j;
+	int length1 = _strlen(s1);
+	int length2 = _strlen(s2);
+	int i, j;
 	char *concatenated;
 
-	if (s1 == NULL)
-	{
-		length1 = 0;
-	}
-	else
-	{
-		length1 = _strlen(s1);
-	}
-	if (s2 == NULL)
-	{
-		length2 = 0;
-	}
-	else
-	{
-		length2 = _strlen(s2);
-	}
 	concatenated = malloc(sizeof(char) * (length1 + length2 + 1));
+
 	if (concatenated == NULL)
-	{
 		return (NULL);
-	}
-	for (i = 0; i <= length1; i++)
+
+	if (s1 != NULL)
 	{
-		concatenated[i] = s1[i];
+		for (i = 0; i < length1; i++)
+			concatenated[i] = s1[i];
 	}
-	for (j = 0; j <= length2; j++)
+
+	if (s2 != NULL)
 	{
-		concatenated[length1 + j] = s2[j];
+		for (j = 0; j < length2; j++)
+			concatenated[length1 + j] = s2[j];
 	}
+
 	concatenated[length1 + length2] = '\0';
-	return (concatenated);
+
+	return concatenated;
 }
