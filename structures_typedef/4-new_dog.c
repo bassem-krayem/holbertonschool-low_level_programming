e0323e67d92c7db81f8743a2859667c6720a55a0
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-16 16:13:11 +0100

    task4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,54 +3,57 @@
 #include <stdlib.h>
 
 /**
- * new_dog -  a function that creates a new dog.
- * @name: the dog name
- * @age: age of the dog
- * @owner: the dog owner
- * Return: dog_t
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: Pointer to the newly created dog.
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int i, name_len, owner_len;
+	int i, name_len = 0, owner_len = 0;
 
 	if (name == NULL || owner == NULL)
-		return NULL;
+		return (NULL);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return NULL;
+		return (NULL);
 
 	/* Calculate lengths of name and owner */
-	for (name_len = 0; name[name_len] != '\0'; name_len++)
+	while (name[name_len++])
 		;
-	for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
+	while (owner[owner_len++])
 		;
 
 	/* Allocate memory for name and owner */
-	dog->name = malloc(sizeof(char) * (name_len + 1));
+	dog->name = malloc(sizeof(char) * (name_len));
 	if (dog->name == NULL)
 	{
 		free(dog);
-		return NULL;
+		return (NULL);
 	}
 
-	dog->owner = malloc(sizeof(char) * (owner_len + 1));
+	dog->owner = malloc(sizeof(char) * (owner_len));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
-		return NULL;
+		return (NULL);
 	}
 
 	/* Copy name and owner characters */
-	for (i = 0; i <= name_len; i++)
+	for (i = 0; i < name_len; i++)
 		dog->name[i] = name[i];
-	for (i = 0; i <= owner_len; i++)
+	dog->name[i] = '\0';
+
+	for (i = 0; i < owner_len; i++)
 		dog->owner[i] = owner[i];
+	dog->owner[i] = '\0';
 
 	dog->age = age;
 
-	return dog;
+	return (dog);
 }
