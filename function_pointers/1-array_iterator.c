439fb9e28be531623effe600e6cd5fff27512263
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-19 13:43:45 +0100

    task1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Execute a given function on each element of an array
+ * @array: Pointer to the array
+ * @size: Size of the array
+ * @action: Pointer to the function to execute on each element
+ * Return: void.
+ */
+
+void array_iterator(int *array, unsigned long size, void (*action)(int))
+{
+	unsigned long i;
+
+	for (i = 0; i <= size; i++)
+	{
+		action(array[i]);
+	}
+}
