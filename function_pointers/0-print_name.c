9f8e7afc7ef0b5c61f9c4fec516e9e58b717cc07
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-19 12:23:50 +0100

    task0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name
+ * @name: the name pointer.
+ * @f: the pointer to the function.
+ * Return: void.
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
