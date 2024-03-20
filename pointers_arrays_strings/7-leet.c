69605ffa2cf0ae0c72f7d688c2779f34629ccf02
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-20 17:47:31 +0100

    task7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,28 +1,32 @@
 #include "main.h"
 
 /**
- * leet - Encode a string to 1337.
+ * leet - Encodes a string into 1337.
  * @s: The string to encode.
  *
  * Return: A pointer to the encoded string.
  */
 char *leet(char *s)
 {
-	char *r = s;						  // Pointer to the original string
-	char a[] = {'a', 'e', 'o', 't', 'l'}; // Array of characters to replace
-	char n[] = {'4', '3', '0', '7', '1'}; // Array of replacement characters
+	char *r = s;						  /* Pointer to the original string */
+	char a[] = {'a', 'e', 'o', 't', 'l'}; /* Array of characters to replace */
+	char n[] = {'4', '3', '0', '7', '1'}; /* Array of replacement characters */
 	int i;
 
 	while (*s)
 	{
 		for (i = 0; i < 5; i++)
 		{
-			// Check if the current character matches any of the characters to replace
+			/**
+			 * Check if the current character
+			 * matches any of the characters to replace
+			 */
 			if (*s == a[i] || *s == a[i] - 32)
-				*s = n[i]; // Replace the character with the corresponding replacement character
+				/* Replace the character with the corresponding replacement character */
+				*s = n[i];
 		}
-		s++; // Move to the next character in the string
+		s++; /* Move to the next character in the string */
 	}
 
-	return (r); // Return a pointer to the modified string
+	return (r); /* Return a pointer to the modified string */
 }
