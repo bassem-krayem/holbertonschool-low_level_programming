5d2d387c723c2f1578901a52b22e494a4c55176c
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-21 10:22:26 +0100

    task3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+int op_add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
+
+int op_sub(int a, int b)
+{
+	int result = a - b;
+
+	return (result);
+}
+
+int op_mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
+
+int op_div(int a, int b)
+{
+	int result = a / b;
+
+	return (result);
+}
+
+int op_mod(int a, int b)
+{
+	int result = a % b;
+
+	return (result);
+}
