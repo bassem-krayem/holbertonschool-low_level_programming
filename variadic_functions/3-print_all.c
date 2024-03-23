fd0496d2dd795d382bfb2823aba12d9497f5baaf
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-23 14:39:10 +0100

    task3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,7 +6,6 @@
  * print_all - Prints anything
  * @format: Format string indicating the types of arguments passed
  */
-
 void print_all(const char *const format, ...)
 {
 	va_list args;
@@ -14,15 +13,10 @@ void print_all(const char *const format, ...)
 	unsigned int i = 0;
 	char current_format;
 
-	/* Initialize variable arguments */
 	va_start(args, format);
-
-	/* Loop through the format string */
 	while (format && format[i])
 	{
-		/* Get the current format specifier */
 		current_format = format[i];
-		/* Check the current format specifier and print accordingly */
 		switch (current_format)
 		{
 		case 'c':
@@ -35,24 +29,18 @@ void print_all(const char *const format, ...)
 			printf("%f", va_arg(args, double));
 			break;
 		case 's':
-			/* Handle NULL string */
 			str = va_arg(args, char *);
 			if (str == NULL)
 				str = "(nil)";
 			printf("%s", str);
 			break;
 		}
-
-		/* Print separator if not the last element */
-		if ((format[i + 1] != '\0') && (current_format == 'c' || current_format == 'i' || current_format == 'f' || current_format == 's'))
+		if ((format[i + 1] != '\0') && (current_format == 'c' ||
+										current_format == 'i' || current_format == 'f' ||
+										current_format == 's'))
 			printf(", ");
-
 		i++;
 	}
-
-	/* Print newline */
 	printf("\n");
-
-	/* Clean up variable arguments */
 	va_end(args);
 }
