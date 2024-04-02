6ff0372f1b670ec165efa27e675cec73f91df42e
bassem-krayem <bassem.krayem09@gmail.com>
2024-04-02 12:29:25 +0100

    task2 Write a function that adds a new node at the beginning of a list_t list.

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,12 +9,17 @@
  *
  * Return: The address of the new element, or NULL if it failed.
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
 
 	if (str == NULL)
 		return (NULL);
+
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -27,7 +32,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
