fcc78deed25cadaf631195347076ba8fa5288e1c
bassem-krayem <bassem.krayem09@gmail.com>
2024-04-02 10:59:15 +0100

    task 0Write a function that prints all the elements of a list_t list.

--- singly_linked_list/0-print_list.c
+++ singly_linked_list/0-print_list.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * print_list - a function that prints  a list
+ * @h: pointer of the list.
+ *
+ * Return: number of the element of the list.
+ */
+
 size_t print_list(const list_t *h)
 {
 	int i;
